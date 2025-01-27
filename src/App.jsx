a70af43873b198a58e33723d7654eebdedd6e92e
kaife0 <mdkaif1590@gmail.com>
2025-01-27 16:05:24 +0530

    updates

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/navbar"
+import NewsBoard from "./components/NewsBoard"
+
+const App = () => {
+  return (
+    <div>
+     <Navbar />
+     <NewsBoard/>
+    </div>
+  )
+}
+
+export default App
