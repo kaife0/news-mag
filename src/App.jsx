88b9444c4199f41c0901bd3f2e48ab9f5e65158f
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-03-08 12:37:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Navbar from "./components/navbar"
 import NewsBoard from "./components/NewsBoard"
 
-const App = () => {
+const App = () => { 
 const [category,setCategory]= useState("general")
 
   return (
