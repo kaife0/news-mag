150132b65ad48e1d847d1ba52518c4767ee2bbdd
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-03-29 19:44:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NewsBoard from "./components/NewsBoard"
 const App = () => { 
 const [category,setCategory]= useState("general")  
 
-  return (
+  return ( 
     <div>
      <Navbar setCategory={setCategory} />
      <NewsBoard category={category} />
